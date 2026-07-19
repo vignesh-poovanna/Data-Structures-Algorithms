b67e9bfdba635c2938d1b717b8cdbf25164b48d9
Vignesh Poovanna A S <vigneshpoovannaas@gmail.com>
2026-07-19 11:20:14 +0530

    Time: 0 ms (100%), Space: 19.4 MB (22.67%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.py
+++ 0832-flipping-an-image/0832-flipping-an-image.py
@@ -3,10 +3,7 @@ class Solution:
         for sublist in image:
             sublist.reverse()
             for i in range(len(sublist)):
-                if sublist[i]==1:
-                    sublist[i]=0
-                else:
-                    sublist[i]=1
+                sublist[i]= sublist[i]^ 1
         
         return image
         
