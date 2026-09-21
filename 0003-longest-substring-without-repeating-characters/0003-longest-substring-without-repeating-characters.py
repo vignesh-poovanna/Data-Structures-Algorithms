bd4dc5b0a0fbeab914175921db917913f2776691
Vignesh Poovanna A S <vigneshpoovannaas@gmail.com>
2026-09-22 00:29:35 +0530

    Time: 394 ms (52.66%), Space: 13.3 MB (81.07%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,24 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        l = 0
-        r = 0
+        l =0
+        r =0
         seen = set()
         ans = 0
 
-        while r < len(s):
-         if s[r] not in seen:
-            seen.add(s[r])
-            r+=1
-            if len(seen)> ans:
-                ans=len(seen)
-         elif s[r] in seen:
-            while s[r] in seen:
-                seen.remove(s[l])
-                l+=1
-            seen.add(s[r])
-            r += 1
-    
-        return ans
+        while r<len(s):
+            if s[r] not in seen:
+                seen.add(s[r])
+                r+=1
+                if len(seen)> ans:
+                    ans = len(seen)
+            elif s[r] in seen:
+                while s[r] in seen:
+                    seen.remove(s[l])
+                    l +=1
+                seen.add(s[r])
+                r += 1
 
-       
+        return ans
